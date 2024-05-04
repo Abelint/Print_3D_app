6ed722bce2ad1e9de1000e31ac8b924cb947b53b
Abelint <abelint19@gmail.com>
2024-05-04 14:45:17 +0700

    DialogIsBadIsea

--- app/src/main/java/com/example/print_3d_app/MainActivity.kt
+++ app/src/main/java/com/example/print_3d_app/MainActivity.kt
@@ -1,26 +1,39 @@
 package com.example.print_3d_app
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity(), FragmentInteractionListener {
-    lateinit var fragInWork: InWork
-    lateinit var fragArchive: Fragment
-    lateinit var tvTest :TextView
+    lateinit var addButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.frameForFragment, InWork()).commit()
 
-        tvTest = findViewById(R.id.textView2)
+        val btnInWork: Button = findViewById(R.id.btn_inwork)
+        btnInWork.setOnClickListener {
+            addButton.visibility = View.VISIBLE
+            supportFragmentManager.beginTransaction().replace(R.id.frameForFragment, InWork()).commit()
+        }
+        val btnArchive: Button = findViewById(R.id.btn_archive)
+        btnArchive.setOnClickListener {
+            addButton.visibility = View.INVISIBLE
+            supportFragmentManager.beginTransaction().replace(R.id.frameForFragment, Archiv()).commit()
+        }
 
-        val addButton: ImageButton = findViewById(R.id.addButton)
+        addButton = findViewById(R.id.addButton)
         addButton.setOnClickListener {
-            tvTest.text = "aegvaegvaergvaer"
-            addNewCardItem()
+            showDialog();
+           // addNewCardItem()
         }
 
     }
@@ -28,9 +41,20 @@ class MainActivity : AppCompatActivity(), FragmentInteractionListener {
     override fun addNewCardItem() {
 
         // Получите ссылку на фрагмент
-       val yourFragment = supportFragmentManager.findFragmentById(R.id.kfragment_in_work) as InWork
+       val yourFragment = supportFragmentManager.findFragmentById(R.id.frameForFragment) as InWork
         // Вызовите функцию addNewCardItem() из фрагмента
         yourFragment.addNewCardItem()
+    }
+
+    fun showDialog(){
+        val dialog : Dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(true)
+        dialog.setContentView(R.layout.dialog_new_order)
+
+        ///////////////////////////////////////////////////////////////////////////добавить логику
+
+        dialog.show()
     }
 
 
