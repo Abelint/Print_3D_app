cf339608cfd9380a14db4cc0d4ea0b2cc2e1c6ea
Abelint <abelint19@gmail.com>
2024-06-15 12:58:45 +0700

    before archiv

--- app/src/main/java/com/example/print_3d_app/Archiv.kt
+++ app/src/main/java/com/example/print_3d_app/Archiv.kt
@@ -5,11 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -17,16 +14,13 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Archiv : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+   private lateinit var listView : ListView
+    private lateinit var archiveLineItems: MutableList<ArchiveLineItem>
+    private lateinit var archiveLineAdapter: ArchiveLineListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,6 +29,12 @@ class Archiv : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_archiv, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
 
     companion object {
@@ -47,13 +47,6 @@ class Archiv : Fragment() {
          * @return A new instance of fragment Archiv.
          */
         // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Archiv().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
